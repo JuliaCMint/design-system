3be32f9ffc3853bfa3057cdf0c2ba9b6412efcad
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 16:22:50 +0500

    added paddings, some styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { StretchableFlexContainer } from './patterns/StretchableFlexContainer'
-import { InlineFlexContainer } from './patterns/InlineFlexContainer'
+import React from "react";
+import { StretchableFlexContainer } from "./patterns/StretchableFlexContainer";
+import { InlineFlexContainer } from "./patterns/InlineFlexContainer";
+import { PaddingWrapper } from "./patterns/PaddingWrapper";
+import styled from "styled-components";
+
+const Menu = styled(StretchableFlexContainer).attrs(() => ({
+  as: PaddingWrapper,
+  padding: ["l", "xl"],
+  gap: "m",
+  stretch: 1,
+}))`
+  background-color: #19141e;
+  color: white;
+`;
 
 const Header = () => {
   return (
-    <>
-    <StretchableFlexContainer stretch={1} align="center" switchAt="40rem">
-        <div>
-       logo
-        </div>
-        <InlineFlexContainer gap="m" justify="center" align="center">
-          <span>About</span>
-          <span>Gallery</span>
-          <span>Events</span>
-          <span>Contact</span>
-        </InlineFlexContainer >
-        <StretchableFlexContainer gap="m" align="center" justify="end">
-          <span>Info</span>
-          <button>Sign In</button>
-        </StretchableFlexContainer>
+    <Menu>
+      <div>logo</div>
+      <InlineFlexContainer gap="m" justify="center" align="center">
+        <span>About</span>
+        <span>Gallery</span>
+        <span>Events</span>
+        <span>Contact</span>
+      </InlineFlexContainer>
+      <StretchableFlexContainer gap="m" align="center" justify="end">
+        <span>Info</span>
+        <button>Sign In</button>
       </StretchableFlexContainer>
-    </>
-  )
-}
+    </Menu>
+  );
+};
 
-export default Header
+export default Header;
