4b3f52e5b8a1f9225cda9b2a00312be9aac36df2
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 19:10:00 +0500

    made flex-wrap property customizable

--- src/components/patterns/InlineFlexContainer.jsx
+++ src/components/patterns/InlineFlexContainer.jsx
@@ -1,11 +1,11 @@
-import styled from "styled-components"
-import {spacingSchema} from "../common/spaces"
+import styled from "styled-components";
+import { spacingSchema } from "../common/spaces";
 
 export const InlineFlexContainer = styled.div`
-    --gap: ${(props)=>spacingSchema[props.gap] ?? spacingSchema.m};
-    display: flex;
-    flex-wrap: wrap;
-    gap: var(--gap);
-    align-items: ${(props)=>props.align ?? "center"};
-    justify-content: ${(props)=>props.justify ?? "flex-start"};
-`
+  --gap: ${(props) => spacingSchema[props.gap] ?? spacingSchema.m};
+  display: flex;
+  flex-wrap: ${(props) => props.wrap ?? "nowrap"};
+  gap: var(--gap);
+  align-items: ${(props) => props.align ?? "center"};
+  justify-content: ${(props) => props.justify ?? "flex-start"};
+`;
