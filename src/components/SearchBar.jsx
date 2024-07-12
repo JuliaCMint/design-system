48e55b24255caf96c749d793e703fb3a03818695
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 19:25:50 +0500

    created search input component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { PaddingWrapper } from "./patterns/PaddingWrapper";
+
+export const SearchBar = styled(PaddingWrapper).attrs(() => ({
+  as: "input",
+  padding: ["m", "l"],
+}))`
+  background-color: rgba(144, 76, 119, 0.4);
+  border: none;
+`;
