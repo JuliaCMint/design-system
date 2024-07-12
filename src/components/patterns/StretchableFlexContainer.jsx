2d5d651c3ce62347c7bbc89bc7b7fe956c3b79fe
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 20:55:39 +0500

    formatted

--- src/components/patterns/StretchableFlexContainer.jsx
+++ src/components/patterns/StretchableFlexContainer.jsx
@@ -14,19 +14,19 @@ const responsive = css`
 `;
 
 const stretchMap = {
-    all: `> *  { flex: 1 }`,
-    start: `> :first-child { flex: 1 }`,
-    end: `> :last-child { flex: 1 }`,
-  };
-  
-  export const StretchableFlexContainer = styled(InlineFlexContainer)`
-    flex-wrap: nowrap;
-    ${({ stretch }) => {
-      if (typeof stretch === "number")
-        return `> :nth-child(${stretch + 1}) { flex: 1 }`;
-  
-      return stretchMap[stretch] ?? "";
-    }}
-  
-    ${({ switchAt }) => switchAt && responsive}
-  `;
+  all: `> *  { flex: 1 }`,
+  start: `> :first-child { flex: 1 }`,
+  end: `> :last-child { flex: 1 }`,
+};
+
+export const StretchableFlexContainer = styled(InlineFlexContainer)`
+  flex-wrap: nowrap;
+  ${({ stretch }) => {
+    if (typeof stretch === "number")
+      return `> :nth-child(${stretch + 1}) { flex: 1 }`;
+
+    return stretchMap[stretch] ?? "";
+  }}
+
+  ${({ switchAt }) => switchAt && responsive}
+`;
