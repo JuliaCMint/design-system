1131895c91d5449b869648565d8600a2e8b4cc33
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 13:28:36 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+const App =() => {
+  return (
+    <h3>Hello</h3>
+  );
+}
+
+export default App;
---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import Content from "./conent";
+import MenuBar from "./menu";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h5></h5>
+    </div>
+  );
+};
+
+export default MainPage;
