ec426f7752afedcf75e698e9169f79df37176200
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 20:56:04 +0500

    added customizable flex-direction prop

--- src/components/patterns/InlineFlexContainer.jsx
+++ src/components/patterns/InlineFlexContainer.jsx
@@ -5,6 +5,7 @@ export const InlineFlexContainer = styled.div`
   --gap: ${(props) => spacingSchema[props.gap] ?? spacingSchema.m};
   display: flex;
   flex-wrap: ${(props) => props.wrap ?? "nowrap"};
+  flex-direction: ${(props) => props.direction ?? "row"};
   gap: var(--gap);
   align-items: ${(props) => props.align ?? "center"};
   justify-content: ${(props) => props.justify ?? "flex-start"};
