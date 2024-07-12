b4fe1931d6665f796644ebb72eb29914f1c0194c
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-12 15:56:46 +0500

    added elements to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { StretchableFlexContainer } from './patterns/StretchableFlexContainer'
+import { InlineFlexContainer } from './patterns/InlineFlexContainer'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <StretchableFlexContainer stretch={1} align="center" switchAt="40rem">
+        <div>
+       logo
+        </div>
+        <InlineFlexContainer gap="m" justify="center" align="center">
+          <span>About</span>
+          <span>Gallery</span>
+          <span>Events</span>
+          <span>Contact</span>
+        </InlineFlexContainer >
+        <StretchableFlexContainer gap="m" align="center" justify="end">
+          <span>Info</span>
+          <button>Sign In</button>
+        </StretchableFlexContainer>
+      </StretchableFlexContainer>
+    </>
   )
 }
 
