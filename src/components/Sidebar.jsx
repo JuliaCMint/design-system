5c44004b50e242c9c9b8b27c5ee82e2cc1673768
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-15 20:51:38 +0500

    created sidebar menu item styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { GridLayers } from "./patterns/GridLayers";
 import { PaddingWrapper } from "./patterns/PaddingWrapper";
+import { Logo } from "./Logo";
+import { InlineFlexContainer } from "./patterns/InlineFlexContainer";
 
 const SideBar = () => {
   const SideMenu = styled(GridLayers).attrs(() => ({
@@ -11,13 +13,31 @@ const SideBar = () => {
     padding-inline-start: 0;
   `;
 
+  const SideMenuItem = styled(InlineFlexContainer).attrs(() => ({
+    as: PaddingWrapper,
+    gap: "l",
+    padding: ["m", "l"],
+    align: "center",
+  }))`
+    border-inline-start: 0.25rem solid transparent;
+    ${(props) =>
+      props.active &&
+      `
+        border-inline-start-color: #19141e;
+        background: #904c7766;
+      `}
+  `;
+
   return (
     <PaddingWrapper padding={["l", "none"]}>
       <SideMenu>
-        <li>General</li>
-        <li>Services</li>
-        <li>Membership</li>
-        <li>Notifications</li>
+        <SideMenuItem active>
+          <Logo square size="1rem" />
+          General
+        </SideMenuItem>
+        <SideMenuItem>Services</SideMenuItem>
+        <SideMenuItem>Membership</SideMenuItem>
+        <SideMenuItem>Notifications</SideMenuItem>
       </SideMenu>
     </PaddingWrapper>
   );
