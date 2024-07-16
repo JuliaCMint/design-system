d550f2d04050a990e711815c7879378c7ece3c17
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-16 19:56:10 +0500

    made title dynamic

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,39 +2,24 @@ const MainContent = ({ selectedMenuItem }) => {
   const renderContent = () => {
     switch (selectedMenuItem) {
       case "General":
-        return (
-          <>
-            <h1>General</h1>
-            <p>This is the general content</p>
-          </>
-        );
+        return <p>This is the general content</p>;
       case "Services":
-        return (
-          <>
-            <h1>Services</h1>
-            <p>This is the services content</p>
-          </>
-        );
+        return <p>This is the services content</p>;
       case "Membership":
-        return (
-          <>
-            <h1>Membership</h1>
-            <p>This is the membership content</p>
-          </>
-        );
+        return <p>This is the membership content</p>;
       case "Notifications":
-        return (
-          <>
-            <h1>Notifications</h1>
-            <p>This is the notifications content</p>
-          </>
-        );
+        return <p>This is the notifications content</p>;
       default:
         return null;
     }
   };
 
-  return <div>{renderContent()}</div>;
+  return (
+    <div>
+      <h1>{selectedMenuItem}</h1>
+      {renderContent()}
+    </div>
+  );
 };
 
 export default MainContent;
