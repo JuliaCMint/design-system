8ac845c5f9803b72faee39cbc3e8b8641b49daf2
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-16 19:54:08 +0500

    render content dynamically

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,40 @@
-const MainContent = () => {
-  return (
-    <div>
-      <h1>Main Content</h1>
-      <p>This is the main content</p>
-    </div>
-  );
+const MainContent = ({ selectedMenuItem }) => {
+  const renderContent = () => {
+    switch (selectedMenuItem) {
+      case "General":
+        return (
+          <>
+            <h1>General</h1>
+            <p>This is the general content</p>
+          </>
+        );
+      case "Services":
+        return (
+          <>
+            <h1>Services</h1>
+            <p>This is the services content</p>
+          </>
+        );
+      case "Membership":
+        return (
+          <>
+            <h1>Membership</h1>
+            <p>This is the membership content</p>
+          </>
+        );
+      case "Notifications":
+        return (
+          <>
+            <h1>Notifications</h1>
+            <p>This is the notifications content</p>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return <div>{renderContent()}</div>;
 };
 
 export default MainContent;
