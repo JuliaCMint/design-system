1b74efd4c1ba6499106884f3a325843b318413f7
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-16 21:10:12 +0500

    added basic form and logo

--- src/components/GeneralContent.jsx
+++ src/components/GeneralContent.jsx
@@ -1,8 +1,32 @@
+import { Logo } from "./Logo";
+import { GridLayers } from "./patterns/GridLayers";
+import { Split } from "./patterns/Split";
+import logoImg from "../images/girl.png";
+
 const GeneralContent = () => {
+  const LabeledInput = ({ label, children }) => {
+    return (
+      <GridLayers as="label" gap="s">
+        {label}
+        {children}
+      </GridLayers>
+    );
+  };
+
   return (
-    <div>
-      <p>This is the general content of the website.</p>
-    </div>
+    <GridLayers>
+      <Split gap="l" fraction="auto-end">
+        <GridLayers>
+          <LabeledInput label="Name">
+            <input type="text" />
+          </LabeledInput>
+          <LabeledInput label="Email">
+            <input type="email" />
+          </LabeledInput>
+        </GridLayers>
+        <Logo image={logoImg} size="10rem" />
+      </Split>
+    </GridLayers>
   );
 };
 export default GeneralContent;
