b5cd32efbb8434380a1c7d607a38324ffd5091da
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-17 21:23:11 +0500

    made responsive

--- src/components/patterns/GridColumns.jsx
+++ src/components/patterns/GridColumns.jsx
@@ -6,4 +6,8 @@ export const GridColumns = styled.div`
   display: grid;
   gap: ${(props) => spacingSchema[props.gap] ?? spacingSchema.l};
   grid-template-columns: repeat(var(--columns), 1fr);
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
