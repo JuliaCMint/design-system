e4162ed21391fdf89094839df392428ff3fe5c79
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-17 15:50:34 +0500

    added more input fields

--- src/components/GeneralContent.jsx
+++ src/components/GeneralContent.jsx
@@ -2,6 +2,7 @@ import { Logo } from "./Logo";
 import { GridLayers } from "./patterns/GridLayers";
 import { Split } from "./patterns/Split";
 import logoImg from "../images/girl.png";
+import { GridColumns } from "./patterns/GridColumns";
 
 const GeneralContent = () => {
   const LabeledInput = ({ label, children }) => {
@@ -17,15 +18,30 @@ const GeneralContent = () => {
     <GridLayers>
       <Split gap="l" fraction="auto-end">
         <GridLayers>
-          <LabeledInput label="Name">
+          <LabeledInput label="Username">
             <input type="text" />
           </LabeledInput>
-          <LabeledInput label="Email">
-            <input type="email" />
+          <LabeledInput label="About">
+            <input type="text" />
           </LabeledInput>
         </GridLayers>
         <Logo image={logoImg} size="10rem" border="1px solid #19141e" />
       </Split>
+
+      <GridColumns gap="l" columns={2}>
+        <LabeledInput label="First Name">
+          <input type="text" />
+        </LabeledInput>
+        <LabeledInput label="Last Name">
+          <input type="text" />
+        </LabeledInput>
+        <LabeledInput label="Company">
+          <input type="text" />
+        </LabeledInput>
+        <LabeledInput label="Role">
+          <input type="text" />
+        </LabeledInput>
+      </GridColumns>
     </GridLayers>
   );
 };
