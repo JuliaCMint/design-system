79517ad7ab482fcdbccc8244f40d9da55a6fc470
JuliaCMint <julia.malikova.jm@gmail.com>
2024-07-18 09:17:04 +0500

    modified button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,9 +5,10 @@ export const Button = styled(PaddingWrapper).attrs(() => ({
   as: "button",
   padding: ["m", "l"],
 }))`
-  background-color: ${(props) => props.backgroundColor ?? "#904C77"};
-  color: ${(props) => props.color ?? "white"};
-  border: none;
+  background-color: ${(props) => (props.primary ? "#904C77" : "transparent")};
+  color: ${(props) => (props.primary ? "white" : "#19141e")};
+  border: ${(props) =>
+    props.primary ? "1px solid transparent" : "1px solid #904C77"};
   border-radius: 0.25rem;
   cursor: pointer;
 
